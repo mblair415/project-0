faa9bbb98f3432a72c231bf1473c5e689e31ad13
mblair415 <mblair415@gmail.com>
2016-12-09 00:13:50 -0800

    foundation for js done.

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,54 @@
-$(document).ready(function() {
-  console.log("js loads")
+// $(document).ready(function() {
+//   console.log("app.js loads")
 
-})
+  function damage(base, low, high){
+    return (Math.floor(Math.random() * (high-low + 1) + low)) + base;
+  }
+
+  $(".btn").on("click", function() {
+    var fightMob = data.mob.difficulty1[0] // eventually decide difficulty level
+    fight(data.player, fightMob);
+  })
+
+  function fight(player, fightMob){
+    $("#enemy").append("<img id="fightMob.image" src="fightMob.image"/>");
+    combat(player, fightMob);
+    // unfinished
+  }
+
+  function combat(player, fightMob){
+    var tempMobHp = fightMob.hp;
+
+    function roundOfCombat(player, fightMob){  // this portion will loop until player or mob is dead
+      var playerSwing = damage(player.dmg);
+      if (tempMobHp - playerSwing < 1) {
+        playerWin(player, fightMob, playerSwing);  // does not exist yet.  exp, level check, loot
+      } else {
+        var mobSwing = damage(fightMob.dmg);
+        tempMobHp -= playerSwing;
+        if (player.hp - damage(fightMob.dmg) < 1) {
+          playerDeath(fightMob); // does not exist yet.  say something on the screen. modal?
+        } else {
+          player.hp -= mobSwing;
+          rountOfCombat(player, fightMob); // recursive loop
+        }
+      }
+    }
+  }
+
+  function playerWin(player, fightMob, playerSwing){
+    console.log("You killed a " + fightMob.name + " with a swing for " +
+    playerSwing + " damage!"); // need a modal or something.
+    // need something for loot.
+    player.xp += fightMob.xp;
+    if (player.xp > 5) { // need to turn this into something more impressive.
+      player.lvl += 1; // player needs to be told.
+      console.log("You gained a level! You're now level " + player.lvl);
+    }
+  }
+
+  function playerDeath(fightMob) { // need a modal or something.
+    
+  }
+
+// })
--- scripts/data.js
+++ scripts/data.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
   console.log("data.js loads")
 
+  var player = {
+    name : "Frodo",  // eventually a field the player can enter a name into
+    level : 1, // a function?  not sure yet.  not sure how i want to handle xp yet.
+    xp : 0,
+    hp : 10,  // eventually an equation.   combination of gear bonuses and level with a modifier for class.
+    dmg : [0, 1, 2]
+    gear : {
+      head : {};
+      arms : {},
+      chest : {},
+      hands : {},
+      legs : {name : "pants of modesty", hp : 0, dmg : 0},
+      feet : {}
+    }
+  }
+
+  var mobs = {
+    difficulty1 : [
+      {
+        name : "small, malnourished, infant rat",
+        image : "http://img.photobucket.com/albums/v661/Pandara1/IRL%20Pets/sideview.jpg",
+        lvl : 1,
+        xp : 1,
+        hp : 5,
+        dmg : [0, 1, 1],
+        loot : "none"
+      }
+    ]
+  }
+
 })
