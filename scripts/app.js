a44d4b5c2d047aeba49e8375c4d79e6caaea80ec
mblair415 <mblair415@gmail.com>
2016-12-11 15:26:04 -0800

    nearly complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   console.log('app.js loads')
 
+// second: do the loot check indepent of the creature.  just run the function
+// to check for loot once a thing dies.
 
 var player = {
   name : 'Frodo',  // eventually a field the player can enter a name into
-  lvl : 1, // a function?  not sure yet.  not sure how i want to handle xp yet.
+  lvl : 1,
   xp : 0,
-  hp : 5,  // eventually an equation.   combination of gear bonuses and level with a modifier for class.
+  hp : 10,  // eventually an equation.   combination of gear bonuses and level with a modifier for class.
   dmg : [0, 1, 4],
   gear : {
     head : {},
@@ -22,20 +24,20 @@ var lootTable = [[ // main array, which nested arrays that correstpond to value
   {
     slot : 'feet',
     name : 'gore covered strips of fur',
-    hp : 2,
+    hp : 0,
     dmg : 1,
     rating : 2
   },
   {
     slot : 'head',
-    name : 'smelly burlap sack with holes in it',
+    name : 'smelly sack with a hole',
     hp : 1,
     dmg : 0,
     rating : 1
   },
   {
     slot : 'arms',
-    name : 'a selfmade friendship bracelet',
+    name : 'a friendship bracelet',
     hp : 0,
     dmg : 0,
     rating : 0
@@ -73,8 +75,8 @@ var lootTable = [[ // main array, which nested arrays that correstpond to value
   {
     slot : 'feet',
     name : 'soft leather moccasins',
-    hp : 4,
-    dmg : 0,
+    hp : 2,
+    dmg : 1,
     rating : 2
   },
   {
@@ -88,23 +90,23 @@ var lootTable = [[ // main array, which nested arrays that correstpond to value
     slot : 'arms',
     name : 'bone and talon bracers',
     hp : 1,
-    dmg : 2,
+    dmg : 1,
     rating : 2
   },
   {
     slot : 'head',
     name : 'fur lined helm',
     hp : 3,
-    dmg : 0
+    dmg : 0,
+    rating : 2
   },
   {
     slot : 'legs',
     name : 'bone and fur greaves',
-    hp : 3,
-    dmg : 3,
+    hp : 2,
+    dmg : 2,
     rating : 3
   }
-
 ]]
 
 var mobs = [
@@ -114,6 +116,33 @@ var mobs = [
       lvl : 1,
       xp : 1,
       hp : 2,
+      dmg : [0, 0, 1],
+      loot : whichLoot(0)
+    },
+    {
+      name : 'infant rat',
+      image : 'http://www.cutestpaw.com/wp-content/uploads/2013/12/baby-rat.jpg',
+      lvl : 2,
+      xp : 2,
+      hp : 8,
+      dmg : [0, 0, 2],
+      loot : whichLoot(0)
+    },
+    {
+      name : 'pacifist rat',
+      image : 'https://s-media-cache-ak0.pinimg.com/236x/a0/73/5b/a0735bef5b5beda13af358a19cde6119.jpg',
+      lvl : 1,
+      xp : 1,
+      hp : 6,
+      dmg : [0, 0, 1],
+      loot : whichLoot(0)
+    },
+    {
+      name : 'crippled mammoth rat',
+      image : 'http://crazeyivan.files.wordpress.com/2010/09/fat-rat1.jpg',
+      lvl : 1,
+      xp : 1,
+      hp : 10,
       dmg : [0, 1, 1],
       loot : whichLoot(0)
     },
@@ -123,7 +152,7 @@ var mobs = [
       lvl : 1,
       xp : 1,
       hp : 5,
-      dmg : [0, 0, 2],
+      dmg : [0, 0, 1],
       loot : whichLoot(0)
     },
     {
@@ -131,8 +160,26 @@ var mobs = [
       image : 'http://pics.livejournal.com/helsic_fangirl/pic/00013h76/s320x240',
       lvl : 2,
       xp : 2,
-      hp : 8,
-      dmg : [0, 2, 3],
+      hp : 9,
+      dmg : [0, 1, 3],
+      loot : whichLoot(1)
+    },
+    {
+      name : 'jaws',
+      image : 'http://news.bbcimg.co.uk/media/images/50060000/jpg/_50060756_rat.jpg',
+      lvl : 2,
+      xp : 2,
+      hp : 6,
+      dmg : [0, 0, 5],
+      loot : whichLoot(1)
+    },
+    {
+      name : 'butters',
+      image : 'http://2.bp.blogspot.com/-0st69Q6xF_4/UAOEhsHLIQI/AAAAAAAALb8/o8ojhZERPi8/s1600/Funny+Rat_.jpg',
+      lvl : 2,
+      xp : 2,
+      hp : 10,
+      dmg : [0, 0, 2],
       loot : whichLoot(1)
     }
 ]
@@ -143,18 +190,18 @@ updateStats(player);
     return (Math.floor(Math.random() * (high-low + 1) + low)) + base;
   }
 
-  $(".btn").on("click", function() {
-    // console.log("test test button test")
-    var fightMob = mobs[0] // eventually decide difficulty level system
-    console.log(fightMob.loot)
+  $(".btn").on("click", function() { // add combat sound
+    var fightMob = whichMob();
     fight(player, fightMob);
   })
 
   function whichLoot(num){
     return lootTable[num][(Math.floor(Math.random() * 6))];
   }
-  console.log(whichLoot(1));
-  console.log(whichLoot(0));
+
+  function whichMob(){
+    return mobs[(Math.floor(Math.random() * mobs.length))]
+  }
 
   function updateStats(player){
     $("#player-level").html('');
@@ -168,37 +215,33 @@ updateStats(player);
     ' - ' + (player.dmg[0] + player.dmg[2]) + '</p>');
   }
 
-  function fight(player, fightMob){  // flash image and trigger combat function
+  function fight(player, fightMob){
     $('#enemy').html('');
     $('#enemy').append('<img id="'+fightMob.image+'" src="'+fightMob.image+'"/>');
     $('#enemy').fadeIn(500).fadeOut(500).fadeIn(500);
+
     combat(player, fightMob);
-    // unfinished
   }
 
   function combat(player, fightMob){ // set up temp hp for mob.  recursive loot for combat
     var tempMobHp = fightMob.hp;
-    // console.log("combat has started")
     $(".player-status-combat").prepend('<pre>You found a ' + fightMob.name + '!</pre>');
     roundOfCombat(player,fightMob, tempMobHp);
 
     function roundOfCombat(player, fightMob, currentMobHp){  // this portion will loop until player or mob is dead
-      console.log("a round of combat has started.  mob has " + tempMobHp)
       var playerSwing = damage(player.dmg);
-      console.log("round start player dmg " + playerSwing + " mob hp " + tempMobHp);
       if (tempMobHp - playerSwing < 1) {
         playerWin(player, fightMob, playerSwing);
         $(".player-status-combat").prepend('<pre>You murdered it with a vicious swing for ' + playerSwing + '!</pre>');
       } else {
         var mobSwing = damage(fightMob.dmg);
-        currentMobHp -= playerSwing;
+        tempMobHp -= playerSwing;
         $(".player-status-combat").prepend('<pre>You hit for ' + playerSwing + '!</pre>');
         if (player.hp - mobSwing < 1) {
           player.hp = 0;
           playerDeath(player, fightMob, mobSwing);
         } else {
           player.hp -= mobSwing;
-          console.log("mob hits for " + mobSwing + " your current health " + player.hp)
           $(".player-status-combat").prepend('<pre>You were hit for ' + mobSwing + '!  You were reduced to ' + player.hp + ' health.</pre>');
           updateStats(player);
           roundOfCombat(player, fightMob, tempMobHp); // recursive loop
@@ -208,32 +251,37 @@ updateStats(player);
   }
 
   function playerWin(player, fightMob, playerSwing){  // if you win, check for gaining things and update screen
-    console.log("You killed a " + fightMob.name + " with a swing for " +
-    playerSwing + " damage!"); // need a modal or something.
     player.xp += fightMob.xp;
     updateGear(player, fightMob);
     updateStats(player);
-    console.log(player.lvl + ' is player level.  and player xp is ' + player.xp)
     if (player.xp > 5 && player.lvl === 1) { // need to turn this into something more impressive.
       player.lvl += 1; // player needs to be told.
       player.hp = player.lvl * 5;
+      // player.dmg[0] += 1;
       updateStats(player);
-      console.log("You gained a level! You're now level " + player.lvl);
+      $(".player-status-event").prepend('<pre>You gained a level and are now level ' + player.lvl + '!</pre>');
     } else if (player.xp > 15 && player.lvl === 2) {
       player.lvl += 1;
       player.hp = player.lvl * 5;
+      // player.dmg[0] += 1;
       updateStats(player);
+      $(".player-status-event").prepend('<pre>You gained a level and are now level ' + player.lvl + '!</pre>');
+    } else if (player.xp > 25 && player.lvl === 3) {
+      $(".player-status-event").prepend('<pre>You kicked all the rats!  You win!</pre>');
+
     }
   }
 
-  function updateGear(player, fightMob){
+  function updateGear(player, fightMob){ // checks to see if new gear is better than current, updates stats and equips on screen
     var mobGearRating = fightMob.loot.rating;
+    var mobGearName = fightMob.loot.name;
     var curSlot = fightMob.loot.slot;
 
     if (mobGearRating === undefined) {
       return;
     } else {
-      if (mobGearRating <= player.gear[curSlot].rating){
+      if (mobGearRating <= player.gear[curSlot].rating ||
+        mobGearName === player.gear[curSlot].name){
         return;
       } else {
         player.gear[curSlot] = fightMob.loot;
@@ -243,26 +291,17 @@ updateStats(player);
         $('#player-gear-' + curSlot).html('');
         $('#player-gear-' + curSlot).append('<p>' + player.gear[curSlot].name +
         '</p>');
-        $(".player-status-event").prepend('<pre>You found ' + fightMob.loot.name + '!</pre>');
+        $('#player-gear-' + curSlot).fadeIn(200).fadeOut(200).fadeIn(200);
+        $(".player-status-event").prepend('<pre>You found ' +
+        fightMob.loot.name + '!</pre>');
       }
     }
   }
 
-  /*
-i want this to update the status window based on what is happening in game.
-
-  */
-
-  // function playerGainLevel(player, fightMob) {  // beyond current scope
-  //   player.xp += fightMob.xp;
-  //   updateStats(player);
-  //   if (player.xp >= 5+(player.lvl*1.15)
-  // }
-
   function playerDeath(player, fightMob, mobSwing) { // need a modal or something.
-    console.log("you died, dude.")
-    $(".player-status-combat").prepend('<pre>You were hit for ' + mobSwing + ' and you succum to the damage.  You have died from your wounds.</pre>');
-    $(".player-status-combat").prepend('<pre>You got all the way to level ' + player.lvl + ' way to go!</pre>');
+    // console.log("you died, dude.")
+    $(".player-status-combat").prepend('<pre>You were hit for ' + mobSwing + ' and you succum to the damage.</pre>');
+    $(".player-status-event").prepend('<pre>You got all the way to level ' + player.lvl + ' before you died!</pre>');
   }
 
 })
